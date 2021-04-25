76b047d8d46125cd427ad5b7ecc676c9ac813d72
sirancheevisan <sirancheevishan@gmail.com>
2021-04-25 18:50:53 +0530

    my 1st commit.

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BPS_Dashboard.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult UserManagement()
+        {
+            return View();
+        }
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+        public ActionResult PaymentDetails()
+        {
+            return View();
+        }
+        public ActionResult ManagementDetails()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
+        public ActionResult Empty()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult MorrisChart()
+        {
+            return View();
+        }
+        public ActionResult Table()
+        {
+            return View();
+        }
+        public ActionResult TabPanel()
+        {
+            return View();
+        }
+        public ActionResult UiElements()
+        {
+            return View();
+        }
+    }
+}
